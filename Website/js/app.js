fcb6d9c70e23db501359c93380fa6ec61976bdd5
Scott Lim <sll009@ucsd.edu>
2015-11-18 09:20:57 -0800

    ADDED ANGULAR- BBBBBRACE YOURSELVES

--- Website/js/app.js
+++ Website/js/app.js
@@ -1,14 +1,1 @@
 var foodieApp = angular.module('app', []);
-
-foodieApp.controller("MainController", function(){
-    var vm = this;
-    vm.title = 'Foodie';
-});
-
-foodieApp.controller("search", function($scope) {
-	$scope.input = '';
-	$scope.get = function(wow) {
-		$scope.input = wow;
-		alert("WOW " + $scope.input);
-	}
-});
