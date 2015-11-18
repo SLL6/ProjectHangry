f38974e646803a48036a8f7276d601601f30b7ce
Sirinya Phakoom <sphakoom@ucsd.edu>
2015-11-18 10:23:46 -0800

    working on routes

--- Website/js/app.js
+++ Website/js/app.js
@@ -1,1 +1,14 @@
 var foodieApp = angular.module('app', []);
+
+app.config(['$routeProvider', function($routes) {
+
+  $routes.when('/',{
+    templateUrl : '../index.html',
+    controller : MainController
+  });
+
+  $routes.otherwise({
+    redirectTo : '/'
+  });
+
+}]);
