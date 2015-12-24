db83cc858ae328fd8ebc1425e1e02688d70b2571
Christian Memije <christianmemije@gmail.com>
2015-12-24 14:33:56 -0800

    Cleaned up. HO HO HO! MERRY CHRISTMAS!

--- README.md
+++
@@ -1,5 +0,0 @@
-#Foodie
-Recipe site that generates recipes from user's ingredients.
-
-
-[View Live](https://rawgit.com/SLL6/ProjectHangry/master/Website/index.html)
---
+++ js/app.js
@@ -0,0 +1,14 @@
+var foodieApp = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routes) {
+
+    $routes.when('/', {
+        templateUrl: 'index.html',
+        controller: 'MainController'
+    });
+
+    $routes.otherwise({
+        redirectTo: '/'
+    });
+
+}]);
